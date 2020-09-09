a8f9be057c576a8504ca59f50e9ad152c1e15349
kholilboy <kholil.zboy@gmail.com>
2020-09-10 00:16:50 +0700

    Add Form Pendaftaran + Style Bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,69 +1,44 @@
-export default {
+module.exports = {
   /*
-   ** Nuxt rendering mode
-   ** See https://nuxtjs.org/api/configuration-mode
-   */
-  mode: 'spa',
-  /*
-   ** Nuxt target
-   ** See https://nuxtjs.org/api/configuration-target
-   */
-  target: 'static',
-  /*
-   ** Headers of the page
-   ** See https://nuxtjs.org/api/configuration-head
-   */
+  ** Headers of the page
+  */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "testbro",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" },
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
+        rel: "stylesheet",
+        type: "text/css",
+        href:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css",
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
-   ** Global CSS
-   */
-  css: ['vuesax/dist/vuesax.css'],
+  ** Customize the progress bar color
+  */
+  loading: { color: "#3B8070" },
   /*
-   ** Plugins to load before mounting the App
-   ** https://nuxtjs.org/guide/plugins
-   */
-  plugins: ['@/plugins/vuesax'],
-  /*
-   ** Auto import components
-   ** See https://nuxtjs.org/api/configuration-components
-   */
-  components: true,
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
-  ],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
-  /*
-   ** Build configuration
-   ** See https://nuxtjs.org/api/configuration-build/
-   */
-  build: {},
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/,
+        });
+      }
+    },
+  },
 };
---
+++ pages/form-pendaftaran.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container" style="margin-top: 5%">
+    <form method="POST" class="my-login-validation" novalidate="">
+      <div class="form-group">
+        <label>NPM</label>
+        <input
+          id="email"
+          type="email"
+          class="form-control"
+          name="email"
+          value=""
+          required
+          autofocus
+          placeholder="170810100xx"
+        />
+      </div>
+
+      <div class="form-group">
+        <label>Nama</label>
+        <input
+          id="password"
+          type="password"
+          class="form-control"
+          name="password"
+          required
+          placeholder="Paijo"
+        />
+      </div>
+
+      <div class="form-group">
+        <label>Email</label>
+        <input
+          id="password"
+          type="password"
+          class="form-control"
+          name="password"
+          required
+          placeholder="paijo@xyz.com"
+        />
+      </div>
+
+      <div class="form-group">
+        <label>Angkatan</label>
+        <input
+          id="password"
+          type="password"
+          class="form-control"
+          name="password"
+          required
+          placeholder="2017"
+        />
+      </div>
+
+      <div class="form-group m-0">
+        <button type="submit" class="btn btn-primary btn-block">
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
