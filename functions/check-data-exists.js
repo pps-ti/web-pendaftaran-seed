752a1c37b62c10dc0d23f4ca271aee9a01941f3e
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-09-27 09:23:05 +0700

    Safer check-data-exists function using try - catch block

--- functions/check-data-exists.js
+++ functions/check-data-exists.js
@@ -11,23 +11,31 @@ exports.handler = async (event, context) => {
     };
   }
 
-  const sheetType = event.queryStringParameters.type;
-  const npm = event.queryStringParameters.npm;
+  try {
+    const sheetType = event.queryStringParameters.type;
+    const npm = event.queryStringParameters.npm;
 
-  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID_FROM_URL);
+    const doc = new GoogleSpreadsheet(
+      process.env.GOOGLE_SPREADSHEET_ID_FROM_URL
+    );
 
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  });
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    });
 
-  await doc.loadInfo();
-  const sheet = doc.sheetsByTitle[sheetType];
-  const rows = await sheet.getRows();
-  const npmExists = rows.some((row) => row.NPM === npm);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByTitle[sheetType];
+    const rows = await sheet.getRows();
+    const npmExists = rows.some((row) => row.NPM === npm);
 
-  return {
-    statusCode: 200,
-    body: String(npmExists),
-  };
+    const unique = !npmExists;
+
+    return {
+      statusCode: 200,
+      body: String(unique),
+    };
+  } catch (error) {
+    throw new Error('Tidak dapat mengambil data');
+  }
 };
