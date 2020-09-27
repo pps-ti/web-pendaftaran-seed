7c23fbb54d7850c5bfc29d53dba27f3dfbe40f73
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-09-27 09:24:08 +0700

    Form - serverless functions integration done

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,25 @@
     <form id="form" @submit.prevent="onSubmit">
       <h1 class="form-title">Pendaftaran <strong>SEED</strong></h1>
       <hr class="form-hr" />
-      <div v-if="formStep == 1" class="form-page form-page--1">
+      <div v-if="formStep === 1" class="form-page form-page--1">
         <vs-row>
-          <vs-input v-model="npm" placeholder="NPM">
-            <template
-              v-if="$v.npm.$error || (!$v.npm.required && npm !== undefined)"
-              #message-danger
-            >
-              Harus diisi
+          <vs-input
+            placeholder="NPM"
+            :value="npm"
+            :loading="checkingNpm"
+            @blur="npm = $event.target.value"
+          >
+            <template #message-danger>
+              <span v-if="$v.npm.$error && !$v.npm.required">
+                Harus diisi
+              </span>
+              <span
+                v-else-if="
+                  !$v.npm.isUnique && !checkingNpm && npm !== undefined
+                "
+              >
+                NPM ini sudah terdaftar
+              </span>
             </template>
           </vs-input>
         </vs-row>
@@ -43,9 +54,9 @@
         </vs-row>
         <label for="gender" class="gender-label">Gender: </label>
         <vs-row class="gender-row">
-          <vs-radio v-model="gender" name="gender" val="L"
-            >Laki - Laki</vs-radio
-          >
+          <vs-radio v-model="gender" name="gender" val="L">
+            Laki - Laki
+          </vs-radio>
           <vs-radio v-model="gender" name="gender" val="P">Perempuan</vs-radio>
         </vs-row>
         <vs-row>
@@ -96,7 +107,7 @@
         </vs-row>
       </div>
 
-      <div v-if="formStep == 2" class="form-page form-page--2">
+      <div v-if="formStep === 2" class="form-page form-page--2">
         <vs-row class="survey-question">
           <label for="survey1">
             1. Apakah Anda sudah familiar dengan HTML dan CSS?
@@ -165,6 +176,17 @@
           <vs-button size="large" class="send-button">Kirim</vs-button>
         </vs-row>
       </div>
+
+      <div v-if="formStep === 3" class="form-page form-page--3">
+        <img
+          class="form-done-img"
+          src="/confirmed.svg"
+          alt="Registration success"
+        />
+
+        Terimakasih telah mendaftar workshop SEED kali ini, silakan cek kotak
+        masuk alamat surel Anda.
+      </div>
     </form>
   </div>
 </template>
@@ -189,12 +211,35 @@ export default {
       survey3: 'Belum',
       survey4: 'Belum',
       survey5: 'Belum',
+      checkingNpm: false,
     };
   },
   validations: {
     npm: {
       required,
       minLength: minLength(4),
+      async isUnique(value) {
+        this.checkingNpm = true;
+
+        try {
+          const unique = Boolean(
+            await this.$axios.$get(
+              `/.netlify/functions/check-data-exists?type=Absensi&npm=${value}`
+            )
+          );
+
+          this.checkingNpm = false;
+          return unique;
+        } catch (error) {
+          this.$vs.notification({
+            color: 'danger',
+            position: 'top-center',
+            title: 'Maaf terjadi kesalahan pada sistem',
+            text: 'Silakan coba kembali beberapa saat lagi',
+            classNotification: 'notif',
+          });
+        }
+      },
     },
     nama: {
       required,
@@ -230,23 +275,63 @@ export default {
       }
     },
   },
+  mounted() {
+    const form = document.getElementById('form');
+
+    form.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter' && this.formStep === 1) {
+        event.preventDefault();
+        this.formStep = 2;
+      }
+    });
+  },
   methods: {
-    onSubmit() {
+    async onSubmit() {
       this.$v.$touch();
       if (this.$v.$invalid) {
-        // this.submitting = true;
         this.openNotification();
       } else {
-        this.openLoading();
-        console.log({
-          nama: this.nama,
-          npm: this.npm,
-          angkatan: this.angkatan,
-          gender: this.gender,
-          bawaLaptop: this.bawaLaptop,
-          surel: this.surel,
-          alasanIkut: this.alasanIkut,
+        const loading = this.$vs.loading({
+          background: '#fff',
+          color: 'primary',
+          opacity: 1,
+          scale: 1,
+          text: 'Sedang mengirim...',
         });
+
+        try {
+          await this.$axios.$post(
+            '/.netlify/functions/post-data?type=Absensi',
+            {
+              npm: this.npm,
+              nama: this.nama,
+              angkatan: this.angkatan,
+              gender: this.gender,
+              surel: this.surel,
+              alasanIkut: this.alasanIkut,
+              survey1: this.survey1,
+              survey2: this.survey2,
+              survey3: this.survey3,
+              survey4: this.survey4,
+              survey5: this.survey5,
+            }
+          );
+
+          await this.$axios.$post('/.netlify/functions/send-mail', {
+            recipient: this.surel,
+          });
+
+          loading.close();
+          this.formStep = 3;
+        } catch (error) {
+          this.$vs.notification({
+            color: 'danger',
+            position: 'top-center',
+            title: 'Maaf terjadi kesalahan pada sistem',
+            text: 'Silakan coba kembali beberapa saat lagi',
+            classNotification: 'notif',
+          });
+        }
       }
     },
   },
